52df83300661f0b3b4a4168c800298a02585687e
Kevin Cheang <cheangk009@gmail.com>
2019-07-17 13:14:38 -0700

    change formatting

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import './App.css'
 
 
-function Todo({todo, index, completeTodo, removeTodo}){
-  return(
-    <div className="todo" style={{textDecoration: todo.isCompleted ? 'line-through' : ''}}> 
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
       {todo.text}
       <div>
         <button onClick={() => completeTodo(index)}> Complete</button>
@@ -15,27 +15,27 @@ function Todo({todo, index, completeTodo, removeTodo}){
   )
 }
 
-function TodoForm({addTodo}) {
-    const [value, setValue] = useState('');
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState('');
 
-    const handleSubmit = e => {
-      e.preventDefault();
-      if(!value) return;
-      addTodo(value);
-      setValue('');
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue('');
+  }
 
-    return (
-      <form onSubmit={handleSubmit}>  
-          <input 
-            type="text" 
-            className="input" 
-            value={value} 
-            onChange={e => setValue(e.target.value)} 
-            placeholder="Add todo"
-          />
-      </form>
-    )
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+        placeholder="Add todo"
+      />
+    </form>
+  )
 }
 
 export default function App() {
@@ -73,18 +73,18 @@ export default function App() {
 
   return (
     <div className="app">
-      <div className="todo-list"> 
-          {todos.map((todo, index) => (
-            <Todo 
-              key={index} 
-              index={index} 
-              todo={todo} 
-              completeTodo={completeTodo}
-              removeTodo={removeTodo}
-            />
-          ))}
-          <TodoForm addTodo={addTodo} />
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
       </div>
-    </div> 
+    </div>
   )
 }
